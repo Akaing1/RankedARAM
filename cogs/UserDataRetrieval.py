2da9fc143e84c2802b6f7638a6437b70cdc21ab3
Anthony <anthony.arcomona@gmail.com>
2025-02-02 18:49:45 -0500

    gitignore now working

---
+++ cogs/UserDataRetrieval.py
@@ -0,0 +1,29 @@
+from discord import app_commands
+from discord.ext import commands
+
+from data.LPManagement.LPManagement import *
+from data.database.dbOperations import *
+
+db = DBOperations()
+
+
+class UserDataRetrieval(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("Data ready to be retrieved!")
+
+    @app_commands.command(name="get_data", description="gets user data from db")
+    async def getData(self, interaction):
+        await interaction.response.send_message(db.getUserData(interaction))
+
+    @app_commands.command(name="lp", description="gets user current LP status")
+    async def LP(self, interaction):
+        await interaction.response.send_message(getLPFromUser(interaction, db))
+
+
+async def setup(bot):
+    await bot.add_cog(UserDataRetrieval(bot))
---
+++ enums/RankEnum.py
@@ -0,0 +1,13 @@
+from enum import Enum
+
+
+class Rank(Enum):
+    IRON = "iron"
+    BRONZE = "bronze"
+    SILVER = "silver"
+    GOLD = "gold"
+    PLATINUM = "platinum"
+    DIAMOND = "diamond"
+    MASTER = "master"
+    GRANDMASTER = "grandmaster"
+    CHALLENGER = "challenger"
