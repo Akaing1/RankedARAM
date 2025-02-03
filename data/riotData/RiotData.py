67f214722765a7e9b82ee35a5d93045b251a678c
Anthony <anthony.arcomona@gmail.com>
2025-02-03 17:40:50 -0500

    getpuuid created

--- data/riotData/RiotData.py
+++ data/riotData/RiotData.py
@@ -8,12 +8,20 @@ API_KEY = os.getenv('RIOT_API_KEY')
 
 
 class RiotData:
+    ACCOUNT_DATA_V1_BY_IGN = 'https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/%s/%s?api_key='
+
     def __init__(self):
         self.API_KEY = os.getenv('RIOT_API_KEY')
 
     def checkRiotIDisValid(self, riotID) -> bool:
-        componentID = (riotID[:riotID.index('#')], riotID[riotID.index('#')+1:])
-        req = 'https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/%s/%s?api_key=' % componentID
+        componentID = (riotID[:riotID.index('#')], riotID[riotID.index('#') + 1:])
+        req = self.ACCOUNT_DATA_V1_BY_IGN % componentID
         if requests.get(req + self.API_KEY).status_code == 200:
             return True
         return False
+
+    def getPuuid(self, riotID) -> str:
+        componentID = (riotID[:riotID.index('#')], riotID[riotID.index('#') + 1:])
+        req = self.ACCOUNT_DATA_V1_BY_IGN % componentID
+        response = requests.get(req + self.API_KEY)
+        return response.json().get('puuid')
